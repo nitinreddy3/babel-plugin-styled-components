917f848ffdc31bb7263c5d79eb8dd9fb713c19ab
Phil Pluckthun <phil.pluckthun@thetrainline.com>
2017-01-28 23:15:36 +0000

    Disable minificiation when preprocessing is enabled

--- src/visitors/minify.js
+++ src/visitors/minify.js
@@ -1,9 +1,19 @@
 import * as t from 'babel-types'
-import { useMinify } from '../utils/options'
+import {
+  useMinify,
+  useCSSPreprocessor
+} from '../utils/options'
 import { isStyled, isHelper } from '../utils/detectors'
 
 export default (path, state) => {
-  if (useMinify(state) && (isStyled(path.node.tag, state) || isHelper(path.node.tag, state))) {
+  if (
+    useMinify(state) &&
+    !useCSSPreprocessor(state) &&
+    (
+      isStyled(path.node.tag, state) ||
+      isHelper(path.node.tag, state)
+    )
+  ) {
     const templateLiteral = path.node.quasi
     for (let element of templateLiteral.quasis) {
       element.value.raw = element.value.raw.replace(/(\\r|\\n|\r|\n)\s*/g, '')
